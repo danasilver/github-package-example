178fdba328c351e5246ffd0bccbbf3610a3d0243
danasilver <dsilver1221@gmail.com>
2025-04-25 19:56:10 -0700

    Remove helloWorldWithName.

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ function helloWorld() {
     console.log("Hello, World!");
 }
 
-function helloWorldWithName(name) {
-    console.log(`Hello, ${name}!`);
-}
-
 module.exports = {
     helloWorld: helloWorld,
-    helloWorldWithName: helloWorldWithName
 };
